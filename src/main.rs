d21472a744f5cfef006f282d87df7d2c90652d65
dsolerh <dsolerh.cinter95@gmail.com>
2023-05-07 00:44:07 +0300

    first server enpoints

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+#[get("/{name}")]
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    HttpResponse::Ok().body(format!("Hello {}!\n", name))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(greet)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
